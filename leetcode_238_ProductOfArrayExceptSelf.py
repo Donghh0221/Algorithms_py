73771deb9723cfc1d577140c1f8a4b25846fe9ae
donghui <ehdgnl101@korea.ac.kr>
2021-08-02 21:34:34 +0900

    Solve: Leetcode238 and Leetcode12

--- leetcode_238_ProductOfArrayExceptSelf.py
+++ leetcode_238_ProductOfArrayExceptSelf.py
@@ -1,13 +1,39 @@
-import math
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
         answer = []
+        p = 1
+        for i in range(len(nums)):
+            answer.append(p)
+            p = p * nums[i]
+
+        p = 1
+        for i in range(len(nums) - 1, -1, -1):
+            answer[i] = answer[i] * p
+            p = p * nums[i]
+
+        return answer
+
+
+class Solution1:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        answer = []
+
+        reversed_nums = list(reversed(nums))
+        product_from_left = [1]
+        product_from_right = [1]
         n = len(nums)
+
         for i in range(n):
-            answer.append(math.prod(nums[0:i])*math.prod(nums[i+1:n]))
+            product_from_right.append(product_from_right[i] * reversed_nums[i])
+            product_from_left.append(product_from_left[i] * nums[i])
+
+        for j in range(n):
+            answer.append(product_from_left[n - j - 1] * product_from_right[j])
+        answer = list(reversed(answer))
         return answer
 
 
 if __name__ == '__main__':
     S = Solution()
-    print(S.productExceptSelf([1,2,3,4]))
+    print(S.productExceptSelf([1, 2, 3, 4]))
+
