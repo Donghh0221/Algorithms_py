ccfb69ee3190a7336f9c971754f60d2744440c6e
donghui <ehdgnl101@korea.ac.kr>
2021-08-04 22:56:27 +0900

    Solve: Leetcode206 and Leetcode21

---
+++ leetcode_152_MaximumProductSubarray.py
@@ -0,0 +1,36 @@
+import math
+import sys
+
+
+class Solution:
+    def maxProduct(self, nums: list[int]) -> int:
+        '''
+        1. 2포인터를 사용
+        2. 브루스포트 -> O(n^2)
+        '''
+        n = len(nums)
+        max_value = -sys.maxsize
+        for i in range():
+            for j in range(1,)
+            print()
+
+
+            0:n
+
+            0:n-1
+            1:n
+
+            0:n-2
+            1:n-1
+            2:n
+            ...
+            0:1
+            n-1:n
+            in range(n-n):
+
+
+
+
+if __name__ == '__main__':
+    S = Solution()
+    S.maxProduct([2,3,-2,4])
---
+++ leetcode_21_MergeTwoSortedLists.py
@@ -0,0 +1,19 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        print(l1, l2)
+        if (not l1) or (l2 and l1.val > l2.val):
+            l1, l2 = l2, l1
+        if l1:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1
+
+
+if __name__ == '__main__':
+    S = Solution()
+    S.mergeTwoLists(l1=ListNode([1, 2, 4]), l2=ListNode([1, 3, 4]))
